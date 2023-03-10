85e4b5b44bb2d156e9f417f630c48f2ae7f617d1
Halo <87004079+Halo-oo@users.noreply.github.com>
2023-03-10 17:02:31 +0900

    Feat: 오늘의 TEST Front단 일부구현
    - 템플릿 일부 구현

---
+++ frontend/src/components/Quiz/BackupQuiz/TempStepProgress.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div>
+      <step-progress
+        :steps="this.mySteps"
+        :current-step="this.currentStep"
+        icon-class="fa fa-check"
+      ></step-progress>
+      <v-btn @click="preStep">이전</v-btn>
+      <v-btn @click="nextStep">다음</v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+import StepProgress from "vue-step-progress";
+
+// import the css (OPTIONAL - you can provide your own design)
+import "vue-step-progress/dist/main.css";
+
+export default {
+  name: "TodayQuiz",
+  components: {
+    "step-progress": StepProgress,
+  },
+  data() {
+    return {
+      mySteps: ["1", "2", "3", "4", "5", "6", "7"],
+      currentStep: 0,
+    };
+  },
+  methods: {
+    nextStep() {
+      this.currentStep++;
+    },
+    preStep() {
+      this.currentStep--;
+    },
+  },
+};
+// ...
+// register the component in your Vue instance
+</script>
--- frontend/src/components/Quiz/TodayQuiz.vue
+++ frontend/src/components/Quiz/TodayQuiz.vue
@@ -1,13 +1,234 @@
 <template>
-  <div>quiz</div>
+  <div>
+    <!-- 프로그레스 바 -->
+    <div><step-progress></step-progress></div>
+    <!-- 스탑워치 -->
+    <div
+      class="stopWatch"
+      v-if="this.correctFlag == null || this.timeoutFlag == null"
+    >
+      <stop-watch></stop-watch>
+    </div>
+    <!-- 정답/오답 결과 출력 -->
+    <div v-if="this.correctFlag == true">정답</div>
+    <div v-else-if="this.correctFlag == false && this.timeoutFlag == false">
+      오답
+    </div>
+    <div v-if="this.timeoutFlag == true">시간 초과</div>
+    <!-- 문제 -->
+    <div class="quizBox" v-if="index < count">
+      <div class="questionTxt">{{ questions[index]["question"] }}</div>
+      <v-card-content
+        class="answerBox"
+        :key="key"
+        v-for="(answer, key) in questions[index]['answers']"
+      >
+        <!-- 원래 Button -->
+        <!-- <v-btn
+          class="px-15 py-15 ma-2"
+          :color="isHovering ? 'var(--quiz-1-col-6)' : 'var(--main-col-4)'"
+          :id="key"
+          :value="key"
+          v-model="selectedAnswer"
+          @click="selectClickAnswer(key)"
+          >{{ answer }}</v-btn
+        > -->
+        <v-btn
+          class="answerBtn"
+          style="height: 150px"
+          :color="`var(--quiz-1-col-6)`"
+          :id="key"
+          :value="key"
+          v-model="selectedAnswer"
+          @click="checkAnswer(key)"
+          >{{ answer }}</v-btn
+        >
+      </v-card-content>
+    </div>
+    <!-- 최종 결과 -->
+    <div v-if="this.index == 7">끝-</div>
+  </div>
 </template>
 
 <script>
-export default {
+import StepProgress from "./StepProgress.vue";
+import StopWatch from "./StopWatch.vue";
 
-}
+export default {
+  components: { StepProgress, StopWatch },
+  name: "TodayQuiz",
+  data() {
+    return {
+      index: 0, // 전체 문제 no
+      time: 10000, // 문제 풀이 시간, 10초(10000)
+      timer: null, // 타이머 interval
+      // timer: setTimeout(this.timeOut, 10000),
+      correctFlag: null, // 현재 문제 정답/오답 판별 Flag
+      timeoutFlag: null, // timeout 판별 Flag
+      correctAnswer: 0, // 정답 개수
+      wrongAnswer: 0, // 오답 개수
+      count: 7, // 문제 수
+      questions: [
+        {
+          question:
+            "1. [ ]란 OECD 기준에 따라 가구를 소득 순으로 나열했을 때, 한가운데에 있는 가구소득(중위소득)의 50~150% 범위에 속한 가구를 뜻한다.",
+          answers: {
+            a: "중산층가구",
+            b: "0.5인 가구",
+            c: "중위소득",
+            d: "4차 산업혁명",
+          },
+          correctAnswer: "a",
+        },
+        {
+          question: "Q. 문제 - 2",
+          answers: {
+            a: "2000",
+            b: "2001",
+            c: "2002",
+            d: "2003",
+          },
+          correctAnswer: "b",
+        },
+        {
+          question: "Q. 문제 - 3",
+          answers: {
+            a: "3000",
+            b: "3001",
+            c: "3002",
+            d: "3003",
+          },
+          correctAnswer: "c",
+        },
+        {
+          question: "Q. 문제 - 4",
+          answers: {
+            a: "4000",
+            b: "4001",
+            c: "4002",
+            d: "4003",
+          },
+          correctAnswer: "d",
+        },
+        {
+          question: "Q. 문제 - 5",
+          answers: {
+            a: "5000",
+            b: "5001",
+            c: "5002",
+            d: "5003",
+          },
+          correctAnswer: "a",
+        },
+        {
+          question: "Q. 문제 - 6",
+          answers: {
+            a: "6000",
+            b: "6001",
+            c: "6002",
+            d: "6003",
+          },
+          correctAnswer: "b",
+        },
+        {
+          question: "Q. 문제 - 7",
+          answers: {
+            a: "7000",
+            b: "7001",
+            c: "7002",
+            d: "7003",
+          },
+          correctAnswer: "c",
+        },
+      ],
+    };
+  },
+  created() {
+    // setTimeout(this.timeOut, 10000);
+    // this.timer();
+    // setInterval(this.timeOut, this.time);
+    this.timer = setInterval(this.timeOut, this.time);
+  },
+  methods: {
+    // [@Method] 선택한 답변 정답 확인
+    checkAnswer(key) {
+      console.log("#21# 선택한 정답 번호: ", key);
+
+      // 선택한 답변 정답 or 오답, 시간 초과에 따라 점수 계산
+      // if (key == this.questions[this.index]["correctAnswer"]) {
+      //   this.correctAnswer++;
+      //   this.correctFlag = true;
+      // } else {
+      //   this.wrongAnswer++;
+      //   this.correctFlag = false;
+      // }
+      // i) 시간 초과
+      if (this.timeoutFlag == true) {
+        this.timeoutFlag = true;
+        this.wrongAnswer++;
+        this.correctFlag = false;
+      }
+      // ii) 정답
+      else if (key == this.questions[this.index]["correctAnswer"]) {
+        this.correctAnswer++;
+        this.correctFlag = true;
+      }
+      // iii) 오답
+      else {
+        this.wrongAnswer++;
+        this.correctFlag = false;
+      }
+      // console.log("#21# 정답 점수: ", this.correctAnswer);
+      // console.log("#21# 오답 점수: ", this.wrongAnswer);
+      console.log("#21# 정답/오답 확인: ", this.correctFlag);
+
+      clearInterval(this.timer);
+      setTimeout(this.nextQuestion, 5000); // 10초 후 다음 문제로 넘어감
+    },
+    // [@Method] 다음 문제로 이동
+    nextQuestion() {
+      this.index++;
+      this.correctFlag = null;
+      this.timeoutFlag = false;
+
+      this.timer = setInterval(this.timeOut, this.time);
+      // 문제 끝 > 결과 출력
+      if (this.index == 7) {
+        this.timeoutFlag = false;
+        clearInterval(this.timer);
+      }
+    },
+    // [@Method] TimeOut에 따른 Data 값 변경
+    timeOut() {
+      if (this.correctFlag == null) {
+        this.wrongAnswer++;
+        this.timeoutFlag = true;
+
+        this.checkAnswer(null);
+      }
+    },
+  },
+};
 </script>
 
 <style>
+.quizBox {
+  margin-left: 5%;
+  margin-right: 5%;
+}
 
+.questionTxt {
+  text-align: center;
+  font-size: larger;
+  letter-spacing: normal;
+  line-height: 28px;
+  margin-bottom: 10%;
+}
+
+.answerBtn {
+  width: 150px;
+  height: 150px;
+  margin-left: 5%;
+  margin-bottom: 5%;
+}
 </style>
