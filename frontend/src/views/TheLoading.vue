af2420e515c850734029700bc44aefd0f6bca322
Halo <87004079+Halo-oo@users.noreply.github.com>
2023-03-22 14:12:32 +0900

    Feat: Axios 통신 시 로딩창 일부 구현
    - 일단 Quiz 로딩창과 동일하게 구현
    - temp

--- frontend/src/views/TheLoading.vue
+++ frontend/src/views/TheLoading.vue
@@ -1,9 +1,24 @@
 <!-- # for. axios 통신 시 Loading 창 -->
-<template>
+<!-- <template>
   <div class="lds-facebook" v-if="loading">
     <div></div>
     <div></div>
     <div></div>
+  </div>
+</template> -->
+<!-- !#! 임시로 해둠 -->
+<template>
+  <div class="main-loading-mask" v-if="loading">
+    <v-card class="main-wrapper">
+      <v-card-text class="main-wrapper-text">
+        <p class="main-exp-title">Quiz 만드는 중</p>
+      </v-card-text>
+      <v-card-text class="main-wrapper-img">
+        <img
+          :src="require('@/assets/images/mypage/quiz/quiz_loading_spinner.gif')"
+        />
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
@@ -18,7 +33,7 @@ export default {
 };
 </script>
 
-<style>
+<!-- <style>
 .lds-facebook {
   display: inline-block;
   position: fixed;
@@ -63,5 +78,51 @@ export default {
     top: 19px;
     height: 26px;
   }
+}
+</style> -->
+<style>
+.main-loading-mask {
+  position: fixed;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
+}
+
+.main-wrapper {
+  width: 70%;
+  height: 30%;
+
+  margin-left: 15%;
+  margin-top: 70%;
+}
+.main-wrapper-text {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  font-family: var(--main-font-2);
+  color: black;
+}
+
+.main-exp-title {
+  font-size: larger;
+  font-weight: 600;
+  color: black;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  margin-top: 5%;
+}
+
+.main-wrapper-img img {
+  width: 250px;
+  height: 100px;
 }
 </style>
