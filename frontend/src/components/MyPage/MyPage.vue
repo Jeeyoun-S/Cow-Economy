bd4a28349940b3724d37e11ab24c958b4fd47d3e
서지윤 <jeey0124@naver.com>
2023-03-22 11:19:46 +0900

    Feat: 마이페이지 전체 틀 구현

--- frontend/src/components/MyPage/MyPage.vue
+++ frontend/src/components/MyPage/MyPage.vue
@@ -3,31 +3,69 @@
     <!-- level profile -->
     <MyPageProfile class="justify-center"></MyPageProfile>
     <!-- username & logout btn -->
-    <v-sheet
-      class="pa-8 d-flex flex-row justify-space-between"
-      rounded="t-xl"
-      elevation="5"
-      min-height="2000px"
-      ><div>
-        <div>안녕하세요</div>
-        <div>홍길동님</div>
+    <v-sheet class="pa-8" rounded="t-xl" elevation="5">
+      <!-- hello & logout -->
+      <v-sheet class="d-flex flex-row justify-space-between pb-5">
+        <div class="xl-font">
+          <div>안녕하세요</div>
+          <div class="d-flex align-center">
+            <span class="b-font">홍길동</span>님<img
+              height="25"
+              class="pl-1"
+              src="@/assets/images/emoji/waving_hand.png"
+            />
+          </div>
+        </div>
+        <MyPageLogoutBtn></MyPageLogoutBtn>
+      </v-sheet>
+      <!-- info & memo -->
+      <div class="swiper-menu">
+        <v-row class="px-2 mt-1 mb-3">
+          <v-col class="pa-1"
+            ><v-btn class="my-info" slot="button-prev" elevation="0" block
+              >나의 정보</v-btn
+            ></v-col
+          ><v-col class="pa-1">
+            <v-btn class="my-memo" slot="button-next" elevation="0" block
+              >나의 메모</v-btn
+            >
+          </v-col>
+        </v-row>
+
+        <swiper class="swiper" :options="swiperOptionMain" ref="swiperMain">
+          <swiper-slide><MyPageInfo></MyPageInfo></swiper-slide>
+          <swiper-slide><MyPageMemo></MyPageMemo></swiper-slide>
+        </swiper>
       </div>
-      <MyPageLogoutBtn></MyPageLogoutBtn>
     </v-sheet>
-    <!-- select -->
-    <div></div>
   </v-sheet>
 </template>
 
 <script>
 import MyPageProfile from "./MyPageProfile.vue";
 import MyPageLogoutBtn from "./MyPageLogoutBtn.vue";
+import MyPageInfo from "./MyPageInfo/MyPageInfo.vue";
+import MyPageMemo from "./MyPageMemo/MyPageMemo.vue";
 
 export default {
   name: "MyPage",
   components: {
     MyPageProfile,
     MyPageLogoutBtn,
+    MyPageMemo,
+    MyPageInfo,
+  },
+  data() {
+    return {
+      swiperOptionMain: {
+        spaceBetween: 10,
+        navigation: {
+          nextEl: ".my-memo",
+          prevEl: ".my-info",
+        },
+      },
+      selectedBtn: "my-memo",
+    };
   },
 };
 </script>
---
+++ frontend/src/components/MyPage/MyPageInfo/MyPageInfo.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>MyPageInfo</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
