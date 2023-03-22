e50a932c2ba2cfd5302f5ec54e6b9d4316902f68
Halo <87004079+Halo-oo@users.noreply.github.com>
2023-03-22 14:10:09 +0900

    Feat: Quiz 진입 시 로딩창 구현
    - chatGPT로부터 유사단어 조회하는 동안
    - 로딩창 출력

--- frontend/src/views/TheLoading.vue
+++ frontend/src/views/TheLoading.vue
@@ -1,5 +1,5 @@
 <!-- # for. axios 통신 시 Loading 창 -->
-<!-- <template>
+<template>
   <div class="lds-facebook" v-if="loading">
     <div></div>
     <div></div>
@@ -63,63 +63,5 @@ export default {
     top: 19px;
     height: 26px;
   }
-}
-</style> -->
-<template>
-  <div class="modal-mask" v-if="loading">
-    <vue-circle
-      :progress="50"
-      :size="100"
-      :reverse="false"
-      line-cap="round"
-      :fill="fill"
-      empty-fill="rgba(0, 0, 0, .1)"
-      :animation-start-value="0.0"
-      :start-angle="0"
-      insert-mode="append"
-      :thickness="5"
-      :show-percent="true"
-      @vue-circle-progress="progress"
-      @vue-circle-end="progress_end"
-    >
-      <p>Slot!</p>
-    </vue-circle>
-  </div>
-  <!-- <div class="loading" v-if="loading">
-    <v-progress-circular
-      :size="70"
-      :width="7"
-      color="primary"
-      indeterminate
-    ></v-progress-circular>
-  </div> -->
-  <!-- <div class="loading" v-if="loading">
-    <v-progress-circular
-      :size="70"
-      :width="7"
-      color="primary"
-      indeterminate
-    ></v-progress-circular>
-  </div> -->
-</template>
-
-<script>
-export default {
-  name: "NowLoading",
-};
-</script>
-
-<style scoped>
-.loading {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 600px;
-  padding: 20px;
-}
-.loading span {
-  font-size: 50px;
-  font-weight: 800;
 }
 </style>
--- frontend/src/views/TheQuizLoading.vue
+++ frontend/src/views/TheQuizLoading.vue
@@ -7,6 +7,20 @@
       <div></div>
     </div>
   </div>
+</template> -->
+<template>
+  <div class="quiz-loading-mask" v-if="loading">
+    <v-card class="quiz-wrapper">
+      <v-card-text class="quiz-wrapper-text">
+        <p class="quiz-exp-title">Quiz 만드는 중</p>
+      </v-card-text>
+      <v-card-text class="quiz-wrapper-img">
+        <img
+          :src="require('@/assets/images/mypage/quiz/quiz_loading_spinner.gif')"
+        />
+      </v-card-text>
+    </v-card>
+  </div>
 </template>
 
 <script>
@@ -21,89 +35,48 @@ export default {
 </script>
 
 <style>
-.lds-facebook {
-  display: inline-block;
-  position: absolute;
+.quiz-loading-mask {
   position: fixed;
-  width: 64px;
-  height: 64px;
-  top: 50%;
-  left: 50%;
+  z-index: 9998;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: table;
+  transition: opacity 0.3s ease;
 }
 
-.lds-facebook div {
-  display: inline-block;
-  position: absolute;
-  left: 6px;
-  width: 13px;
-  background: #5176fa;
-  animation: lds-facebook 1.2s cubic-bezier(0, 0.5, 0.5, 1) infinite;
+.quiz-wrapper {
+  width: 70%;
+  height: 30%;
+
+  margin-left: 15%;
+  margin-top: 70%;
+}
+.quiz-wrapper-text {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  font-family: var(--main-font-2);
+  color: black;
 }
 
-.lds-facebook div:nth-child(1) {
-  left: 6px;
-  animation-delay: -0.24s;
+.quiz-exp-title {
+  font-size: larger;
+  font-weight: 600;
+  color: black;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  margin-top: 5%;
 }
 
-.lds-facebook div:nth-child(2) {
-  left: 26px;
-  animation-delay: -0.12s;
+.quiz-wrapper-img img {
+  width: 250px;
+  height: 100px;
 }
-
-.lds-facebook div:nth-child(3) {
-  left: 45px;
-  animation-delay: 0;
-}
-
-@keyframes lds-facebook {
-  0% {
-    top: 6px;
-    height: 51px;
-  }
-
-  50%,
-  100% {
-    top: 19px;
-    height: 26px;
-  }
-}
-</style> -->
-<!-- <template>
-  <div class="modal-mask" v-if="loading">
-    <vue-circle
-      :progress="50"
-      :size="100"
-      :reverse="false"
-      line-cap="round"
-      :fill="fill"
-      empty-fill="rgba(0, 0, 0, .1)"
-      :animation-start-value="0.0"
-      :start-angle="0"
-      insert-mode="append"
-      :thickness="5"
-      :show-percent="true"
-      @vue-circle-progress="progress"
-      @vue-circle-end="progress_end"
-    >
-      <p>로오딩</p>
-    </vue-circle>
-  </div>
-</template> -->
-<template>
-  <div class="modal-mask" v-if="loading">
-    <div><img :src="require('@/assets/images/mypage/quiz/fail.gif')" /></div>
-  </div>
-</template>
-
-<script>
-export default {
-  props: {
-    loading: {
-      type: Boolean,
-      required: true,
-    },
-  },
-};
-</script>
-
-<style></style>
+</style>
