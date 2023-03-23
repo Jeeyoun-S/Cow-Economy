a02e3f78371990f370bfc8a38f65d0b54b5e45a3
Halo <87004079+Halo-oo@users.noreply.github.com>
2023-03-23 11:33:54 +0900

    Refactor: logout 버튼 동작 주석처리
    - develop에 merge할 건데
    - 로그아웃 아직 구현중 이라 주석 처리

--- frontend/src/components/MyPage/MyPageLogoutBtn.vue
+++ frontend/src/components/MyPage/MyPageLogoutBtn.vue
@@ -1,5 +1,11 @@
 <template>
-  <v-btn class="px-6" elevation="0" color="grey lighten-1" rounded dark @click="kakaoLogOut"
+  <v-btn
+    class="px-6"
+    elevation="0"
+    color="grey lighten-1"
+    rounded
+    dark
+    @click="kakaoLogOut"
     >로그아웃</v-btn
   >
 </template>
@@ -10,14 +16,14 @@ import { mapGetters, mapActions } from "vuex";
 export default {
   name: "MyPageLogoutBtn",
   computed: {
-    ...mapGetters("userStore", ["isLoggedIn"])
+    ...mapGetters("userStore", ["isLoggedIn"]),
   },
   methods: {
     ...mapActions("userStore", ["executeLogOut"]),
     async kakaoLogOut() {
-      await this.executeLogOut();
-    }
-  }
+      // await this.executeLogOut();
+    },
+  },
 };
 </script>
 
