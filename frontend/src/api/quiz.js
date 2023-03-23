96daeea969f3a69198947a88339c853a21b913c5
Halo <87004079+Halo-oo@users.noreply.github.com>
2023-03-23 13:53:00 +0900

    Comment: cosole log 주석 처리

--- frontend/src/api/quiz.js
+++ frontend/src/api/quiz.js
@@ -4,7 +4,7 @@ const api = apiInstance();
 
 // [POST] 오늘의 Quiz로 출제할 경제 단어 가져오기
 async function getQuizWords(userId, success, fail) {
-  console.log("#user - getQuizWords# params - userId: ", userId);
+  // console.log("#user - getQuizWords# params - userId: ", userId);
 
   await api
     .post(`/api/quiz/`, JSON.stringify(userId))
