faea9a3a352fe1f86a6540483b8906dbdbbcfe2b
Halo <87004079+Halo-oo@users.noreply.github.com>
2023-03-24 09:17:40 +0900

    Refactor, Comment: chatGPT 이름, 주석 변경
    - controller 이름 변경
    - 주석 수정

--- backend/src/main/java/com/coweconomy/api/controller/ChatGPTController.java
+++ backend/src/main/java/com/coweconomy/api/controller/ChatGPTController.java
@@ -13,14 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/chatGPT")
-public class Gpt3Controller {
+public class ChatGPTController {
 
-    private static final Logger logger = LoggerFactory.getLogger(Gpt3Controller.class);
+    private static final Logger logger = LoggerFactory.getLogger(ChatGPTController.class);
 
     private final MyChatGPTService myChatGPTService;
 
     @Autowired
-    public Gpt3Controller(MyChatGPTService myChatGPTService) {
+    public ChatGPTController(MyChatGPTService myChatGPTService) {
         this.myChatGPTService = myChatGPTService;
     }
 
@@ -30,7 +30,7 @@ public class Gpt3Controller {
      */
     @PostMapping("/askChatGPT")
     public BaseResponse<?> generateCompletion(@RequestBody ChatGPTRequest chatGPTRequest) {
-        logger.info("#[Gpt3Controller]# 해당 경제용어와 유사한 경제용어 3개 조회 동작 - message: {}", chatGPTRequest);
+//        logger.info("#[Gpt3Controller]# 해당 경제용어와 유사한 경제용어 3개 조회 동작 - message: {}", chatGPTRequest);
 
         try {
             String result = myChatGPTService.getChatResponse(chatGPTRequest.getMessage());
