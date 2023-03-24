2e79ed9c0abfc0b8f94cae61608b9bea79439763
서지윤 <jeey0124@naver.com>
2023-03-24 14:43:36 +0900

    Feat: 마이페이지 사용자 메모 조회 API 구현

--- backend/src/main/java/com/coweconomy/api/controller/UserInfoController.java
+++ backend/src/main/java/com/coweconomy/api/controller/UserInfoController.java
@@ -1,6 +1,8 @@
 package com.coweconomy.api.controller;
 
 import com.coweconomy.api.response.BaseResponse;
+import com.coweconomy.domain.article.dto.ArticleMemoDto;
+import com.coweconomy.domain.article.entity.Article;
 import com.coweconomy.domain.user.dto.UserArticleMemoDto;
 import com.coweconomy.domain.user.entity.UserArticleMemo;
 import com.coweconomy.service.UserInfoService;
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @CrossOrigin
@@ -27,12 +31,21 @@ public class UserInfoController {
         // 임시로 사용자 ID를 1로 설정 (로그인 구현 완료 후, 수정 예정)
         long userId = 1;
 
-        List<UserArticleMemo> memoList = userInfoService.getUserMemo(userId);
-        List<UserArticleMemoDto> memoDtoList = new ArrayList<>();
-        for (UserArticleMemo userArticleMemo : memoList) {
-            memoDtoList.add(new UserArticleMemoDto(userArticleMemo));
-        }
+        // 리턴할 Data 값은 Map 형식으로 지정
+        Map<String, Object> returnMap = new HashMap<>();
 
-        return BaseResponse.success(memoDtoList);
+        // 1. 사용자 정보
+        
+        // 2. 사용자 메모
+        // 사용자가 작성한 모든 메모 가져오기
+        List<UserArticleMemo> memoList = userInfoService.getUserMemo(userId);
+        // 메모를 기사별로 분류하기
+        Map<Article, List<UserArticleMemo>> memoListByGroup = memoList.stream().collect(Collectors.groupingBy(UserArticleMemo::getArticle));
+        // 분류된 기사 Entity Map을 Dto List로 바꾸기
+        List<ArticleMemoDto> articleMemoDtoList = memoListByGroup.entrySet().stream().map(entry -> new ArticleMemoDto(entry.getKey(), entry.getValue())).collect(Collectors.toList());
+        // 결과값에 넣어주기
+        returnMap.put("memo", articleMemoDtoList);
+
+        return BaseResponse.success(returnMap);
     }
 }
