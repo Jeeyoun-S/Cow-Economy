84ee555c8a9a771e03eada16296eadb38b14863e
unknown <1109ssh@naver.com>
2023-03-27 13:01:25 +0900

    Refactoring: 로그인 FE refactoring

--- frontend/src/store/modules/userStore.js
+++ frontend/src/store/modules/userStore.js
@@ -55,9 +55,3 @@ const userStore = {
 };
 
 export default userStore;
-    //   try {
-    //     const accessToken = localStorage.getItem("access-token");
-
-    //     // 카카오 로그아웃 엔드포인트로 POST 요청 보내기
-    //     const response = await this.$
-    //   }
