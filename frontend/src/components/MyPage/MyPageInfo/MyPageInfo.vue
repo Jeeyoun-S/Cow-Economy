4020dfd64b4b18937b2a8d6815bf3363aec4ff54
unknown <1109ssh@naver.com>
2023-03-29 17:02:03 +0900

    S08P22A509-88 Feat: FE Mypage 3번째 경제 옹여 카테고리 그래프

--- frontend/src/components/MyPage/MyPageInfo/MyPageInfo.vue
+++ frontend/src/components/MyPage/MyPageInfo/MyPageInfo.vue
@@ -2,18 +2,22 @@
   <div>
     <ReadNews></ReadNews>
     <ReadNewsCategory></ReadNewsCategory>
+    <WordCategory></WordCategory>
   </div>
 </template>
 
 <script>
 import ReadNews from "@/components/MyPage/MyPageInfo/ReadNews.vue"
 import ReadNewsCategory from "@/components/MyPage/MyPageInfo/ReadNewsCategory.vue"
+import WordCategory from "@/components/MyPage/MyPageInfo/WordCategory.vue"
+
 
 export default {
   name: "MyPageInfo",
   components: {
     ReadNews,
     ReadNewsCategory,
+    WordCategory,
   }
 };
 </script>
