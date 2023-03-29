89dcc77c28570fec5638228931b60e527668f147
dyeon7310 <dyeon7310@gmail.com>
2023-03-29 09:25:14 +0900

    Feat: 홈페이지 인기뉴스, 카테고리별 뉴스 컴포넌트 추가

---
+++ frontend/src/components/HomePage/CategoryNews.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-sheet class="pa-6" color="white">
+    <div class="main-title-font align-center">
+      <img height="19.47" :src="require('@/assets/images/color-tags.png')" />
+      카테고리별 뉴스
+    </div>
+    <div class="main-subtitle-font">
+      최근 24시간 내 카테고리별 뉴스를 확인해 보세요.
+    </div>
+    <div class="pa-4">
+      <span>분류</span>
+      <v-slide-group>
+            <v-chip-group
+              selected-class="text-primary"
+              column
+            >
+              <v-chip filter
+                v-for="tag in tags"
+                :key="tag"
+                color="var(--main-col-2)" outlined>
+                {{ tag }}
+              </v-chip>
+            </v-chip-group>
+          </v-slide-group>
+          </div>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  name: "HotTrends",
+  data() {
+    return {
+      tags: [
+      '금융',
+      '증권',
+      '산업/재계',
+      '중기/벤처',
+      '부동산',
+      '글로벌경제',
+      '생활경제',
+      '경제일반'
+    ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/src/components/HomePage/HotNews.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-sheet class="pa-6" color="white">
+    <div class="main-title-font align-center">
+      <img height="19.47" :src="require('@/assets/images/increase-stats.png')" />
+      인기 뉴스
+    </div>
+    <div class="main-subtitle-font">
+      24시간 내 경제 분야에서 인기 뉴스를 확인해 보세요.
+    </div>
+    <v-sheet class="mt-4 trend_area" rounded color="white">
+      <div id="article">
+        <img>
+      </div>
+    </v-sheet>
+  </v-sheet>
+</template>
+
+<script>
+export default {
+  name: "HotTrends",
+  data() {
+    return {
+      
+    };
+  },
+};
+</script>
+
+<style scoped>
+.trend_area {
+  text-align: center;
+  width: 338;
+  height: 229;
+}
+.trend_img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
