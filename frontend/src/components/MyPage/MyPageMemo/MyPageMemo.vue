cbfe1d1e49016c45ec5942fef93cbc533a874d2c
서지윤 <jeey0124@naver.com>
2023-03-30 14:05:27 +0900

    Feat: 마이페이지 작성한 메모가 없는 경우 메세지 뜨도록 설정

--- frontend/src/components/MyPage/MyPageMemo/MyPageMemo.vue
+++ frontend/src/components/MyPage/MyPageMemo/MyPageMemo.vue
@@ -1,42 +1,50 @@
 <template>
   <div>
     <div class="pa-1 memo d-flex flex-column">
-      <v-row class="pb-3">
-        <v-col cols="4"
-          ><v-select
-            v-model="range"
-            :items="['전체', '기사 제목', '기사 내용', '메모 내용', '인용구']"
-            dense
-            hide-details
-          ></v-select
-        ></v-col>
-        <v-col>
-          <v-text-field
-            v-model="keyword"
-            placeholder="검색어를 입력해 주세요"
-            dense
-            hide-details
-            append-icon="mdi-magnify"
-          ></v-text-field
-        ></v-col>
-      </v-row>
-      <div class="pb-1 ml-auto" v-if="keyword">
-        <v-btn
-          color="var(--main-col-2)"
-          elevation="0"
-          small
-          dark
-          rounded
-          outlined
-          @click="refreshSearch()"
-          ><v-icon class="mr-1" small>mdi-refresh</v-icon>검색 초기화</v-btn
-        >
+      <div
+        v-if="!memoDtoList || memoDtoList.length <= 0"
+        class="align-self-center"
+      >
+        등록된 메모가 없습니다.
       </div>
-      <MyPageMemoList
-        :keyword="keyword"
-        :range="range"
-        :memoDtoList="memoDtoList"
-      ></MyPageMemoList>
+      <div v-else>
+        <v-row class="pb-3">
+          <v-col cols="4"
+            ><v-select
+              v-model="range"
+              :items="['전체', '기사 제목', '기사 내용', '메모 내용', '인용구']"
+              dense
+              hide-details
+            ></v-select
+          ></v-col>
+          <v-col>
+            <v-text-field
+              v-model="keyword"
+              placeholder="검색어를 입력해 주세요"
+              dense
+              hide-details
+              append-icon="mdi-magnify"
+            ></v-text-field
+          ></v-col>
+        </v-row>
+        <div class="pb-1 ml-auto" v-if="keyword">
+          <v-btn
+            color="var(--main-col-2)"
+            elevation="0"
+            small
+            dark
+            rounded
+            outlined
+            @click="refreshSearch()"
+            ><v-icon class="mr-1" small>mdi-refresh</v-icon>검색 초기화</v-btn
+          >
+        </div>
+        <MyPageMemoList
+          :keyword="keyword"
+          :range="range"
+          :memoDtoList="memoDtoList"
+        ></MyPageMemoList>
+      </div>
     </div>
   </div>
 </template>
