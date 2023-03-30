cae79676ae5772b439315aa1ed71f10e8c459d9d
Halo <87004079+Halo-oo@users.noreply.github.com>
2023-03-30 13:45:57 +0900

    Feat: 404Error 페이지 일부 구현

--- frontend/src/components/ErrorAndRending/The404Error.vue
+++ frontend/src/components/ErrorAndRending/The404Error.vue
@@ -17,35 +17,221 @@
   </v-sheet>
 </template> -->
 <template>
-  <v-sheet>
+  <div class="container">
     <home-header />
-    <div class="error-page">
-      <h1 data-h1="404">404</h1>
-      <p data-p="NOT FOUND">NOT FOUND</p>
+    <div class="content">
+      <div id="particles-js">
+        <div class="error-page">
+          <h1 :data-h1="errorCode">404</h1>
+          <p :data-p="errorMessage">NOT FOUND</p>
+        </div>
+      </div>
     </div>
-  </v-sheet>
+    <the-footer />
+  </div>
 </template>
 
 <script>
+import particlesJS from "particles.js";
 import HomeHeader from "@/views/Header/HomeHeader.vue";
+import TheFooter from "@/views/TheFooter.vue";
 
 export default {
-  components: { HomeHeader },
-  name: "The404Error",
-  data: () => ({
-    isShowHeader: true,
-    scrollPosition: 0,
-  }),
+  components: { HomeHeader, TheFooter },
+  mounted() {
+    particlesJS("particles-js", {
+      particles: {
+        number: {
+          value: 5,
+          density: {
+            enable: true,
+            value_area: 800,
+          },
+        },
+        color: {
+          value: "#fcfcfc",
+        },
+        shape: {
+          type: "circle",
+        },
+        opacity: {
+          value: 0.5,
+          random: true,
+          anim: {
+            enable: false,
+            speed: 1,
+            opacity_min: 0.2,
+            sync: false,
+          },
+        },
+        size: {
+          value: 140,
+          random: false,
+          anim: {
+            enable: true,
+            speed: 10,
+            size_min: 40,
+            sync: false,
+          },
+        },
+        line_linked: {
+          enable: false,
+        },
+        move: {
+          enable: true,
+          speed: 8,
+          direction: "none",
+          random: false,
+          straight: false,
+          out_mode: "out",
+          bounce: false,
+          attract: {
+            enable: false,
+            rotateX: 600,
+            rotateY: 1200,
+          },
+        },
+      },
+      interactivity: {
+        detect_on: "canvas",
+        events: {
+          onhover: {
+            enable: false,
+          },
+          onclick: {
+            enable: false,
+          },
+          resize: true,
+        },
+      },
+      retina_detect: true,
+    });
+  },
 };
 </script>
 
-<style>
-.error-page {
-  display: flex;
-  align-items: center;
+<!-- <style lang="scss" scoped>
+.container {
+  // height: 100vh;
+  // display: flex;
   justify-content: center;
-  text-align: center;
-  height: 100%;
-  /* font-family: Arial,"Helvetica Neue",Helvetica,sans-serif; */
+  align-items: center;
+  .content {
+    text-align: center;
+    h1 {
+      font-size: 120px;
+      font-weight: bold;
+      margin: 0;
+      color: #f44336;
+      text-shadow: 0 4px 20px rgba(0, 0, 0, 0.4);
+    }
+    p {
+      font-size: 24px;
+      margin: 0;
+      margin-top: 20px;
+      color: #757575;
+    }
+  }
 }
+</style> -->
+<style lang="sass">
+.error-page
+  display: flex
+  align-items: center
+  justify-content: center
+  text-align: center
+  height: 100%
+  font-family: Arial,"Helvetica Neue",Helvetica,sans-serif
+
+  h1
+    font-size: 30vh
+    font-weight: bold
+    position: relative
+    margin: -8vh 0 0
+    padding: 0
+
+    &:after
+      content: attr(data-h1)
+      position: absolute
+      top: 0
+      left: 0
+      right: 0
+      color: transparent
+      /* webkit only for graceful degradation to IE */
+      background: -webkit-repeating-linear-gradient(-45deg, #71b7e6, #69a6ce, #b98acc, #ee8176, #b98acc, #69a6ce, #9b59b6)
+      -webkit-background-clip: text
+      -webkit-text-fill-color: transparent
+      background-size: 400%
+      text-shadow: 1px 1px 2px transparentize(#fff, .75)
+      animation: animateTextBackground 10s ease-in-out infinite
+
+    + p
+      color: #d6d6d6
+      font-size: 8vh
+      font-weight: bold
+      line-height: 10vh
+      max-width: 600px
+      position: relative
+
+      &:after
+        content: attr(data-p)
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        color: transparent
+        text-shadow: 1px 1px 2px transparentize(#fff, .5)
+        -webkit-background-clip: text
+        -moz-background-clip: text
+        background-clip: text
+
+#particles-js
+  position: fixed
+  top: 0
+  right: 0
+  bottom: 0
+  left: 0
+
+@keyframes animateTextBackground
+  0%
+    background-position: 0 0
+
+  25%
+    background-position: 100% 0
+
+  50%
+    background-position: 100% 100%
+
+  75%
+    background-position: 0 100%
+
+  100%
+    background-position: 0 0
+
+@media (max-width: 767px)
+
+  .error-page h1
+    font-size: 32vw
+
+    + p
+      font-size: 8vw
+      line-height: 10vw
+      max-width: 70vw
+
+a.back
+  position: fixed
+  right: 40px
+  bottom: 40px
+  background: -webkit-repeating-linear-gradient(-45deg, #71b7e6, #69a6ce, #b98acc, #ee8176)
+  border-radius: 5px
+  box-shadow: 0 2px 10px rgba(0,0,0,0.2)
+  color: #fff
+  font-size: 16px
+  font-weight: bold
+  line-height: 24px
+  padding: 15px 30px
+  text-decoration: none
+  transition: 0.25s all ease-in-out
+  &:hover
+    box-shadow: 0 4px 20px rgba(0,0,0,0.4)
 </style>
