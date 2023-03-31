78cd6e42c0a578dff56512e196685b1df5fecab6
민동주 <ods1room@gmail.com>
2023-03-31 13:17:15 +0900

    Feat: 관련 기사 불러오기 구현

--- backend/src/main/java/com/coweconomy/repository/RelatedArticleRepository.java
+++ backend/src/main/java/com/coweconomy/repository/RelatedArticleRepository.java
@@ -2,14 +2,16 @@ package com.coweconomy.repository;
 
 import com.coweconomy.domain.article.entity.RelatedArticle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RelatedArticleRepository extends JpaRepository <RelatedArticle, Long> {
     /**
      * related Article Id로 related_article Table에서 값 찾기
      * @param relatedArticleId
      * @return Optional<RelatedArticle>
      */
-    Optional<RelatedArticle> findByRelatedArticleId(Long relatedArticleId);
+    Optional<RelatedArticle> findByArticle_ArticleId(Long relatedArticleId);
 }
