dee316599cd52bf69f79c07112c8afa95c04a3cb
민동주 <ods1room@gmail.com>
2023-03-31 13:42:10 +0900

    Feat: 관련 기사 불러오기 화면 구현

--- frontend/src/components/NewsDetail/NewsDetailRelation/NewsDetailRelation.vue
+++ frontend/src/components/NewsDetail/NewsDetailRelation/NewsDetailRelation.vue
@@ -3,7 +3,11 @@
     <h3 class="mb-4">관련 기사</h3>
     <swiper class="swiper pt-10 pa-1" :options="swiperOption">
       <div class="swiper-pagination" slot="pagination"></div>
-      <swiper-slide v-for="(relation, index) in relations" :key="index">
+      <swiper-slide
+        style="height: auto"
+        v-for="(relation, index) in newsRelated"
+        :key="index"
+      >
         <NewsDetailRelationItem
           :index="index"
           :relation="relation"
@@ -15,66 +19,16 @@
 
 <script>
 import NewsDetailRelationItem from "./NewsDetailRelationItem.vue";
-
 export default {
   name: "NewsDetailRelation",
+  props: {
+    newsRelated: Array,
+  },
   components: {
     NewsDetailRelationItem,
   },
   data() {
     return {
-      relations: [
-        {
-          title: "비트코인, 연준 '비둘기' 발언에도 3100만원대 횡보",
-          thumbnails:
-            "https://imgnews.pstatic.net/image/005/2023/03/13/2023020520421142396_1675597331_0924291337_20230313000404714.jpg?type=w647",
-        },
-        {
-          title: "비트코인, 연준 '비둘기' 발언에도 3100만원대 횡보",
-          thumbnails:
-            "https://imgnews.pstatic.net/image/005/2023/03/13/2023020520421142396_1675597331_0924291337_20230313000404714.jpg?type=w647",
-        },
-        {
-          title: "비트코인, 연준 '비둘기' 발언에도 3100만원대 횡보",
-          thumbnails:
-            "https://imgnews.pstatic.net/image/005/2023/03/13/2023020520421142396_1675597331_0924291337_20230313000404714.jpg?type=w647",
-        },
-        {
-          title: "비트코인, 연준 '비둘기' 발언에도 3100만원대 횡보",
-          thumbnails:
-            "https://imgnews.pstatic.net/image/005/2023/03/13/2023020520421142396_1675597331_0924291337_20230313000404714.jpg?type=w647",
-        },
-        {
-          title: "비트코인, 연준 '비둘기' 발언에도 3100만원대 횡보",
-          thumbnails:
-            "https://imgnews.pstatic.net/image/005/2023/03/13/2023020520421142396_1675597331_0924291337_20230313000404714.jpg?type=w647",
-        },
-        {
-          title: "비트코인, 연준 '비둘기' 발언에도 3100만원대 횡보",
-          thumbnails:
-            "https://imgnews.pstatic.net/image/005/2023/03/13/2023020520421142396_1675597331_0924291337_20230313000404714.jpg?type=w647",
-        },
-        {
-          title: "비트코인, 연준 '비둘기' 발언에도 3100만원대 횡보",
-          thumbnails:
-            "https://imgnews.pstatic.net/image/005/2023/03/13/2023020520421142396_1675597331_0924291337_20230313000404714.jpg?type=w647",
-        },
-        {
-          title: "비트코인, 연준 '비둘기' 발언에도 3100만원대 횡보",
-          thumbnails:
-            "https://imgnews.pstatic.net/image/005/2023/03/13/2023020520421142396_1675597331_0924291337_20230313000404714.jpg?type=w647",
-        },
-        {
-          title: "비트코인, 연준 '비둘기' 발언에도 3100만원대 횡보",
-          thumbnails:
-            "https://imgnews.pstatic.net/image/005/2023/03/13/2023020520421142396_1675597331_0924291337_20230313000404714.jpg?type=w647",
-        },
-        {
-          title: "비트코인, 연준 '비둘기' 발언에도 3100만원대 횡보",
-          thumbnails:
-            "https://imgnews.pstatic.net/image/005/2023/03/13/2023020520421142396_1675597331_0924291337_20230313000404714.jpg?type=w647",
-        },
-      ],
       swiperOption: {
         slidesPerView: 2,
         spaceBetween: 25,
--- frontend/src/components/NewsDetail/NewsDetailRelation/NewsDetailRelationItem.vue
+++ frontend/src/components/NewsDetail/NewsDetailRelation/NewsDetailRelationItem.vue
@@ -1,43 +1,43 @@
 <template>
-  <v-card
-    class="blue-shadow"
-    width="100%"
-    :height="cardHeight"
-    rounded="default"
-  >
-    <!-- thumbnail -->
-    <img
-      class="swiper-lazy"
-      :data-src="relation.thumbnails"
-      ref="imgEle"
-      height="120"
+  <v-sheet height="100%">
+    <v-card
+      v-if="relation.articleThumbnail != 'NaN'"
+      class="blue-shadow"
       width="100%"
-    />
-    <!-- title -->
-    <div class="pt-1 pa-2 sm-font">
-      {{
-        relation.title.length < 30
-          ? relation.title
-          : relation.title.slice(0, 30) + "···"
-      }}
-    </div>
-  </v-card>
+      height="100%"
+      rounded="default"
+    >
+      <!-- thumbnail -->
+      <div>
+        <img
+          class="swiper-lazy"
+          :src="relation.articleThumbnail"
+          height="120"
+          width="100%"
+        />
+        <!-- title -->
+        <div class="pt-1 pa-2 sm-font">
+          {{
+            relation.articleTitle.length < 30
+              ? relation.articleTitle
+              : relation.articleTitle.slice(0, 30) + "···"
+          }}
+        </div>
+      </div> </v-card
+    ><v-sheet height="100%" v-else>
+      <div class="pt-1 pa-2 sm-font">
+        {{ relation.articleTitle }}
+      </div>
+    </v-sheet>
+  </v-sheet>
 </template>
 
 <script>
 export default {
   name: "NewsDetailRelationItem",
-  computed: {
-    cardHeight() {
-      return document.clientWidth * 0.5;
-    },
-  },
   props: {
     relation: Object,
     index: Number,
-  },
-  mounted() {
-    this.$refs.imgEle.src = this.relation.thumbnails;
   },
 };
 </script>
