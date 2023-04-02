d4a0293c7a2be726ba1d11ccdb45b7c0f5fb9020
dyeon7310 <dyeon7310@gmail.com>
2023-04-02 15:30:45 +0900

    Feat: ArticleDto 기사 썸네일 멤버변수 추가

--- backend/src/main/java/com/coweconomy/domain/article/dto/ArticleDto.java
+++ backend/src/main/java/com/coweconomy/domain/article/dto/ArticleDto.java
@@ -3,30 +3,26 @@ package com.coweconomy.domain.article.dto;
 import com.coweconomy.domain.article.entity.Article;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Data
 @NoArgsConstructor
 public class ArticleDto {
-
     // 기사 ID
     private Long articleId;
-    
     // 기사 작성일
     private String articleRegtime;
-
     // 언론사
     private String articlePress;
-    
     // 기사 제목
     private String articleTitle;
-
+    // 기사 썸네일
+    private String articleThumbnail;
     public ArticleDto(Article article) {
         this.articleId = article.getArticleId();
         this.articleRegtime = article.getArticleRegtime().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));;
         this.articlePress = article.getArticlePress();
         this.articleTitle = article.getArticleTitle();
+        this.articleThumbnail = article.getArticleThumbnail();
     }
 }
