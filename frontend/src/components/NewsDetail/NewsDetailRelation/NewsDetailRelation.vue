f86b4410a775c797a088a05e380c372afdd467cb
민동주 <ods1room@gmail.com>
2023-04-03 11:06:00 +0900

    Feat: 관련 기사 화면 수정

--- frontend/src/components/NewsDetail/NewsDetailRelation/NewsDetailRelation.vue
+++ frontend/src/components/NewsDetail/NewsDetailRelation/NewsDetailRelation.vue
@@ -1,24 +1,29 @@
 <template>
-  <v-sheet id="relation-article" class="py-7" color="var(--main-col-5)">
-    <h3 class="mb-4 pl-7">관련 기사</h3>
-    <v-slide-group class="pt-10" show-arrows>
-      <v-slide-item
-        class="mx-2"
-        style="height: auto"
-        v-for="(relation, index) in newsRelated"
-        :key="index"
-      >
-        <NewsDetailRelationItem
-          :index="index"
-          :relation="relation"
-        ></NewsDetailRelationItem>
-      </v-slide-item>
-    </v-slide-group>
+  <v-sheet id="relation-article" class="pa-5" color="var(--main-col-5)">
+    <div class="swiper-container">
+      <span class="b-font lg-font">관련 기사</span>
+      <div class="swiper-pagination" slot="pagination"></div>
+      <div class="pt-3 swiper-wrapper">
+        <div
+          class="swiper-slide"
+          v-for="(relation, index) in newsRelated"
+          :key="index"
+          style="height: auto; cursor: pointer"
+        >
+          <NewsDetailRelationItem
+            :index="index"
+            :relation="relation"
+          ></NewsDetailRelationItem>
+        </div>
+      </div>
+    </div>
   </v-sheet>
 </template>
 
 <script>
+import Swiper from "swiper";
 import NewsDetailRelationItem from "./NewsDetailRelationItem.vue";
+
 export default {
   name: "NewsDetailRelation",
   props: {
@@ -27,10 +32,18 @@ export default {
   components: {
     NewsDetailRelationItem,
   },
-  data() {
-    return {};
+  mounted() {
+    // Initialize the Swiper instance
+    new Swiper(".swiper-container", {
+      // Add the clickable option
+      clickable: true,
+      slidesPerView: 2,
+      spaceBetween: 20,
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
   },
-  methods: {},
 };
 </script>
 
--- frontend/src/components/NewsDetail/NewsDetailRelation/NewsDetailRelationItem.vue
+++ frontend/src/components/NewsDetail/NewsDetailRelation/NewsDetailRelationItem.vue
@@ -1,33 +1,32 @@
 <template>
-  <v-sheet @click="goNewsDetail" width="150" height="100%">
-    <v-card
-      v-if="relation.articleThumbnail != 'NaN'"
-      class="blue-shadow"
-      width="100%"
-      height="100%"
-      rounded="default"
-    >
+  <v-sheet @click="goNewsDetail" width="100%" height="100%">
+    <v-card class="blue-shadow" width="100%" height="100%" rounded="default">
       <!-- thumbnail -->
       <v-img
-        class="swiper-lazy img-card"
+        v-if="relation.articleThumbnail != 'NaN'"
+        class="swiper-lazy"
         :src="relation.articleThumbnail"
-        height="150"
-        width="100%"
+        height="120"
         cover
-      >
-        <div class="pa-2 sm-font text-in-image">
-          {{
-            relation.articleTitle.length < 25
-              ? relation.articleTitle
-              : relation.articleTitle.slice(0, 25) + "···"
-          }}
-        </div>
-      </v-img>
-
+      ></v-img>
+      <v-img
+        v-else
+        class="swiper-lazy"
+        src="@/assets/images/news_background.jpg"
+        height="120"
+        cover
+      ></v-img>
       <!-- title -->
-    </v-card>
-    <v-card height="100%" class="pt-1 pa-2 sm-font" v-else>
-      <p class="pt-3">{{ relation.articleTitle }}</p>
+      <v-card-text
+        class="pa-2 sm-font"
+        style="width: 90%; !important; color:black"
+      >
+        {{
+          relation.articleTitle.length < 28
+            ? relation.articleTitle
+            : relation.articleTitle.slice(0, 28) + "···"
+        }}
+      </v-card-text>
     </v-card>
   </v-sheet>
 </template>
@@ -49,16 +48,14 @@ export default {
 </script>
 
 <style scoped>
-.no-image-card {
-  /* background: linear-gradient(180deg, rgba(63, 88, 178, 0.5) 0%, #3f58b3 100%); */
-  box-shadow: 0px 0px 5px 1px rgba(0, 0, 0, 0.14);
-  /* color: white; */
-}
-.text-in-image {
+.text_in_img {
   position: absolute;
-  width: 100%;
   bottom: 0;
   color: white;
+  width: 95%;
   background: linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, #000000 84.9%);
+}
+.non_image {
+  background-color: var(--main-col-4-1);
 }
 </style>
