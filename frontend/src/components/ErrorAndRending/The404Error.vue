3d77a6d14510e350c3ceb0573bb2d4bcf861e0b6
Halo <87004079+Halo-oo@users.noreply.github.com>
2023-04-03 10:01:44 +0900

    Design: 404 에러 페이지 디자인 수정

--- frontend/src/components/ErrorAndRending/The404Error.vue
+++ frontend/src/components/ErrorAndRending/The404Error.vue
@@ -2,16 +2,24 @@
   <div>
     <home-header />
     <div class="error-image">
-      <img :src="require('@/assets/images/error-404.gif')" />
+      <img :src="require('@/assets/images/error-404.svg')" />
     </div>
   </div>
+  <!-- <v-sheet>
+    <home-header />
+    <v-sheet class="">
+      <div><img :src="require('@/assets/images/error-404.svg')" /></div>
+    </v-sheet>
+    <the-footer />
+  </v-sheet> -->
 </template>
 
 <script>
 import HomeHeader from "@/views/Header/HomeHeader.vue";
+import TheFooter from "@/views/TheFooter.vue";
 
 export default {
-  components: { HomeHeader },
+  components: { HomeHeader, TheFooter },
 };
 </script>
 
@@ -19,5 +27,7 @@ export default {
 .error-image {
   margin-top: 0%;
   margin-left: 20%;
+
+  width: 80%;
 }
 </style>
