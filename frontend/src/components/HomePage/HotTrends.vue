749e253a1dc439a9b3f070a87b0d91e8183d25b6
서지윤 <jeey0124@naver.com>
2023-04-05 12:23:19 +0900

    Feat: 홈페이지 디자인 수정

--- frontend/src/components/HomePage/HotTrends.vue
+++ frontend/src/components/HomePage/HotTrends.vue
@@ -1,13 +1,22 @@
 <template>
   <v-sheet class="pa-6" color="transparent">
-    <div class="main-title-font align-center">
-      <img height="19.47" :src="require('@/assets/images/emoji _fire.png')" />
-      최신 트렌드
+    <div class="d-flex flex-row align-start">
+      <img
+        height="22"
+        class="mr-1"
+        :src="require('@/assets/images/emoji _fire.png')"
+      />
+      <span class="main-title-font">최신 트렌드</span>
     </div>
     <div class="main-subtitle-font">
       최근 경제 뉴스에서 다루고 있는 핵심 키워드를 확인해 보세요.
     </div>
-    <v-sheet class="mt-4 trend_area pa-4"  rounded="lg" elevation="12" color="white">
+    <v-sheet
+      class="mt-4 trend_area pa-4"
+      rounded="lg"
+      elevation="12"
+      color="white"
+    >
       <img class="trend_img" :src="`${imgPath}`" alt="word cloud 이미지" />
     </v-sheet>
   </v-sheet>
